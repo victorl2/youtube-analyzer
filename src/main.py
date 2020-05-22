c274c200f541d3f97a2c096d3abfd80da517d285
victor silva <victorfts@id.uff.br>
2020-05-22 08:53:47 -0300

    updating the process of getting the video from youtube in a more efficient way ( speding less credits in the api )

--- src/main.py
+++ src/main.py
@@ -1,28 +1,62 @@
 import json
 import os
+import sys
 
 from youtube_api import get_youtube_videos
 
-spreadsheet_id = '1PrnugvoV5hTwHl4Oi6PC-AySKTjvVJc2OBRHuuYVdps'
-youtube_channels = [
-    'UC70YG2WHVxlOJRng4v-CIFQ',
-    ''
-]
-
 
 def main():
-    if 'youtube_api_key' not in os.environ:
-        print('The environment variable "youtube_api_key" is not defined, the key is required to run the app')
-
-    videos = get_videos_web(youtube_channels)
+    verify_variables()
+    videos = get_videos_web("../channels.txt")
     print('{} videos were scrapped from the web'.format(len(videos)))
 
 
-def get_videos_web(channels):
+def get_videos_web(channels_file_name):
     videos = []
-    for channel_id in youtube_channels:
-        videos.extend(get_youtube_videos(channel_id))
+    with open('../output_video_log.txt', 'a') as output:
+        log(output, 'Begining to parse youtube videos\n')
+        """Get a list of objects representing youtube videos."""
+
+        channels_id = get_channels_id(channels_file_name)
+        for channel_id in channels_id:
+
+            channel_videos = []  # get_youtube_videos(channel_id)
+            # videos.extend(channel_videos)
+
+            log(output, '[{}] a total of {} videos were processed for the channel.\n'.format(
+                channel_id, len(channel_videos)))
+        log(output, 'Complete, {} youtube channels were parsed resulting in {} videos\n'.format(
+            len(channels_id), len(videos)))
     return videos
+
+
+def log(output, message):
+    print(message)
+    output.write(message)
+
+
+def get_channels_id(channels_file_name):
+    """Get a list of channels id."""
+    channels = []
+    try:
+        channels_file = open(channels_file_name)
+        channels = [line.strip() for line in channels_file.readlines()]
+    except IOError:
+        print("A file is containing the channels is required to be present in the the project, the app needs to know which channels to get the videos from.")
+        sys.exit()
+    finally:
+        channels_file.close()
+    return channels
+
+
+def verify_variables():
+    """Check if all the variables needed to run the app are defined"""
+    if 'youtube_api_key' not in os.environ:
+        print('The environment variable "youtube_api_key" is not defined, the key is required to run the app')
+        sys.exit()
+    if 'sheet_id' not in os.environ:
+        print('The environment variable "sheet_id" is not defined, a spreadsheet is required to run the app')
+        sys.exit()
 
 
 if __name__ == '__main__':
--- src/youtube_api.py
+++ src/youtube_api.py
@@ -6,9 +6,11 @@ import requests
 """ get youtube api from environment variable """
 youtube_api_key = os.environ['youtube_api_key']
 
+channel_videos_amount = 200
+
 
 class YTVideo():
-    def __init__(self, video_id, channel_id):
+    def __init__(self, video_id, channel_id=None):
         self.video_id = video_id
         self.channel_id = channel_id
         snippet = self.__get_stats_video(video_id)
@@ -22,7 +24,7 @@ class YTVideo():
         self.tags = snippet['tags'] if 'tags' in snippet else []
 
     def __get_stats_video(self, video_id):
-        """ get the latest 50 videos from a youtube channel """
+        """ get the statistics for a given video videos from youtube"""
         response = requests.get(
             'https://www.googleapis.com/youtube/v3/videos?key={}&part=statistics&part=snippet&part=id&id={}'.format(youtube_api_key, video_id))
         video = response.json()['items'][0]
@@ -39,8 +41,19 @@ class YTVideo():
 
 
 def get_youtube_videos(channel_id):
-    """ get the latest 50 videos from a youtube channel """
-    response = requests.get('http://www.mocky.io/v2/5ec63f403200005e00d74bcf')
-    # 'https://www.googleapis.com/youtube/v3/search?key={}&channelId={}&fields=,items(id(videoId))&part=id&maxResults=2'.format(youtube_api_key, channel_id))
+    """ get the latest 200 videos from a youtube channel """
+    response = requests.get(
+        'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&key={}&maxResults={}&playlistId={}'.format(youtube_api_key, channel_videos_amount, __get_id_upload_playlist(channel_id)))
     videos = response.json()
-    return [YTVideo(video['id']['videoId'], channel_id) for video in videos['items']]
+    return [YTVideo(video['contentDetails']['videoId'], channel_id) for video in videos['items']]
+
+
+def get_youtube_video(video_id):
+    return YTVideo(video_id)
+
+
+def __get_id_upload_playlist(channel_id):
+    """ get the id from the playlist containing all videos of a channel """
+    response = requests.get(
+        'https://www.googleapis.com/youtube/v3/channels?key={}&part=contentDetails&id={}'.format(youtube_api_key, channel_id))
+    return response.json()['items']['contentDetails']['uploads']
